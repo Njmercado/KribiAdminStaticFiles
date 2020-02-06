7a99b1d6b96ca4deaaf59ab670a3dd6d2d43a95d
Nino Jesus Mercado Consuegra <njmercado@uninorte.edu.co>
2020-02-06 11:45:31 -0500

    Static server for kribi admin

---
+++ public/js/chunk-2d0b5f94.d2003cdb.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b5f94"],{"1ad2":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" estamos trabajando en esta parte, así que no se preocupe ")])},s=[],o={name:"Historial",methods:{},components:{}},r=o,u=t("2877"),c=Object(u["a"])(r,a,s,!1,null,null,null);n["default"]=c.exports}}]);
+//# sourceMappingURL=chunk-2d0b5f94.d2003cdb.js.map
---
+++ public/precache-manifest.8d6da1904430e8a83bfb5b4e2c7d0aa3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e0c5478ca17e89a0aa",
+    "url": "/css/about.e21aacaf.css"
+  },
+  {
+    "revision": "95c58df765710e77231d",
+    "url": "/css/chunk-1d44e072.638efad3.css"
+  },
+  {
+    "revision": "280359a7dd39fbf48c3d",
+    "url": "/css/chunk-5c7887d0.e21aacaf.css"
+  },
+  {
+    "revision": "442bc5b6391c0a9e73e8",
+    "url": "/css/chunk-5edf8c70.e21aacaf.css"
+  },
+  {
+    "revision": "0766bd01d99027e4e949",
+    "url": "/css/chunk-68c81e66.232c20ea.css"
+  },
+  {
+    "revision": "c24cecb42c11a7e78832",
+    "url": "/css/chunk-vendors.daed7b0b.css"
+  },
+  {
+    "revision": "68cb0cc9ffa3bc2d89acb0347907f669",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86e0c5478ca17e89a0aa",
+    "url": "/js/about.28064b07.js"
+  },
+  {
+    "revision": "07e4bdacec2a24125abe",
+    "url": "/js/app.4f37ae54.js"
+  },
+  {
+    "revision": "95c58df765710e77231d",
+    "url": "/js/chunk-1d44e072.f9182e36.js"
+  },
+  {
+    "revision": "9360736df0f745618c00",
+    "url": "/js/chunk-2d0b5f94.d2003cdb.js"
+  },
+  {
+    "revision": "8b8fb93653e1d58c0609",
+    "url": "/js/chunk-3e7f72ba.33b98b3b.js"
+  },
+  {
+    "revision": "280359a7dd39fbf48c3d",
+    "url": "/js/chunk-5c7887d0.e74179bc.js"
+  },
+  {
+    "revision": "442bc5b6391c0a9e73e8",
+    "url": "/js/chunk-5edf8c70.484253f9.js"
+  },
+  {
+    "revision": "0766bd01d99027e4e949",
+    "url": "/js/chunk-68c81e66.17e737b2.js"
+  },
+  {
+    "revision": "c24cecb42c11a7e78832",
+    "url": "/js/chunk-vendors.c58dd157.js"
+  },
+  {
+    "revision": "318f8b89e2edae166b924dbdfb449902",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
